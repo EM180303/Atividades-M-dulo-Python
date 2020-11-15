448d017a047daa3cdc031782171cd18a9e8f6d5c
Edu Marques <e.marques180303@gmail.com>
2020-11-14 22:28:19 -0300

    Primeira questão

--- Q1.py
+++ Q1.py
@@ -1,7 +1,7 @@
 x = float(input('Digite o valor de x: '))
 y = float(input('Digite o valor de y: '))
 
-print('Dividendo: '+str(x))
-print('Divisor: '+str(y))
-print('Quociente: '+str(x/y))
-print('Resto: ')
+print('Dividendo: ',x)
+print('Divisor: ',y)
+print('Quociente: ',(x / y))
+print('Resto: ',(x % y))
