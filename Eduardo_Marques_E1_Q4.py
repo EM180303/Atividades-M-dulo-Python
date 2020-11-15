ecbf60d9b7525f3ff6276da591b54e7d3ff05907
Edu Marques <e.marques180303@gmail.com>
2020-11-15 15:14:43 -0300

    Quarta qustão

--- Eduardo_Marques_E1_Q4.py
+++ Eduardo_Marques_E1_Q4.py
@@ -1,11 +1,25 @@
 import math
+import random
+from random import Random
 from test.test_import.data.unwritable import x
 letras = []
+frutas = []
+frutas.append('laranja')
+frutas.append('banana')
+frutas.append('melancia')
+frutas.append('uva')
+frutas.append('jaca')
+frutas.append('pinha')
+frutas.append('goiaba')
+frutas.append('manga')
+frutas.append('caju')
+frutas.append('abacate')
 
+f = (math.ceil(10 * random.random()))
 print('*********************************')
 print('*** Jogo da Forca!***')
 print('*********************************')
-palavra_secreta = 'bananas'
+palavra_secreta = frutas[f]
 letras_acertadas = []
 for i in palavra_secreta:
     letras_acertadas.append('_')
