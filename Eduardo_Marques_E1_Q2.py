39c70bf382020193731b23110ef0bec0ceed627c
Edu Marques <e.marques180303@gmail.com>
2020-11-15 15:37:05 -0300

    Primeira atividade de Python

--- Eduardo_Marques_E1_Q2.py
+++ Eduardo_Marques_E1_Q2.py
@@ -1,5 +1,5 @@
 valor = float(input('Qual o valor? '))
-taxa = float(input('Qual a taxa? '))
+taxa = float(input('Qual a taxa (procrntagem de juros)? '))
 tempo = int(input('Qual o tempo, em meses? '))
 prestacao = valor + (valor * (taxa / 100) * tempo)
 print('A prestação é: ', prestacao)
