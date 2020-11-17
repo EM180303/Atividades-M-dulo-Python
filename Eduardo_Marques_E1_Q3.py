0349e8b58fc987880c4848cd6d7c03504640f1dc
Edu Marques <e.marques180303@gmail.com>
2020-11-17 18:45:37 -0300

    Primeira atividade Python

--- Eduardo_Marques_E1_Q3.py
+++ Eduardo_Marques_E1_Q3.py
@@ -5,6 +5,6 @@ frequencia = float(input('Qual a frequência do aluno em % ? '))
 
 media = ((n1 + n2 + n3) / 3)
 if frequencia >= 75 and media >= 7:
-    print('A média do aluno é: ', media, ' e ele foi aprovado.')
+    print('A média do aluno é: ', "%.2f" %media, ' e ele foi aprovado.')
 else:
-    print('A média do aluno é: ', media, ' e ele foi reprovado.')
+    print('A média do aluno é: ', "%.2f" % media, ' e ele foi aprovado.')
