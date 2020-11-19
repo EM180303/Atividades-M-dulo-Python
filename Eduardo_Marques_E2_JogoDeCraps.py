386074327189620085d271ff5b4456adc11b774e
Edu Marques <e.marques180303@gmail.com>
2020-11-19 19:28:14 -0300

    Segunda atividade de Python

--- Eduardo_Marques_E2_JogoDeCraps.py
+++ Eduardo_Marques_E2_JogoDeCraps.py
@@ -2,6 +2,7 @@ import random
 import math
 from random import Random
 import os
+import time
 
 pontuação = 0
 
@@ -12,8 +13,12 @@ pergunta = str(input('Quer ver as regras? (S para sim - N para não)'))
 pergunta = pergunta.upper()
 print(pergunta)
 if (pergunta == 'S'):
+    time.sleep(1)
+    os.system("cls")
     print('Regras:')
-    print('Você jogará 2 dados; Se a soma dos 2 for = 7 ou 11, você ganhou o jogo; se for = 3, 2 ou 12, você perdeu;se não for nenhum desses, o número dos   demais serão os seu ponto, você deve tirar ele novamente para pontuar, até que você tire 7 e o  jogo encerra')
+    print('Você jogará 2 dados; Se a soma dos 2 for = 7 ou 11, você ganhou o jogo; se for = 3, 2 ou 12, você perdeu;se não for nenhum desses, o número dos   demais serão os seu ponto, você deve tirar ele novamente para pontuar, até que você tire 7 e o  jogo encerra.')
+else:
+    os.system
 
 print('Preparado?')
 botao = input('Digite ENTER para jogar os dados')
@@ -23,6 +28,9 @@ result = (dado1 + dado2)
 print('Primeiro dado: ',dado1)
 print('Segundo dado: ',dado2)
 print('Você tirou:',result)
+
+time.sleep(2)
+os.system("cls")
 
 if ((result == 7) or (result == 11)):
     print("Você é um Natural")
@@ -34,6 +42,9 @@ else :
    print('Para pontuar, você deve tirar:',result,' novamente.' )
    verificador = False
    while (verificador == False):
+       time.sleep(2)
+       os.system("cls")
+       print('Para pontuar, você deve tirar:',result,' novamente.' )
        botao = input('Digite ENTER para jogar os dados')
        dado1 = (math.ceil(6 * random.random())) 
        dado2 = (math.ceil(6 * random.random()))
@@ -43,13 +54,16 @@ else :
        print('Você tirou:',result2)
        
        if (result2 == result):
-           pontuação += 1
-           print('Você fez um ponto')
-           print('Pontuação: ',pontuação)
+            time.sleep(2)
+            os.system("cls")
+            pontuação += 1
+            print('Você fez um ponto')
+            print('Pontuação: ',pontuação)
        elif (result2 == 7):
+           time.sleep(2)
+           os.system("cls")
            print('Você perdeu!')
            print('Você fez ',pontuação,' pontos')
            verificador = True
     
-
-       
+print('Fim do jogo')
