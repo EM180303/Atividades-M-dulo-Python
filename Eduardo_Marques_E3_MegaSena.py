235a94598cf8940fd51a06dfd0168919f1a23f9c
Edu Marques <e.marques180303@gmail.com>
2020-11-26 16:02:35 -0300

    Sorteando os palpites

--- Eduardo_Marques_E3_MegaSena.py
+++ Eduardo_Marques_E3_MegaSena.py
@@ -1,1 +1,15 @@
+import random
+import math
+
 jogos = int(input('Quantos palpites você vai querer? '))
+
+Ltemp = []
+Palpites = []
+
+for c in range(jogos):
+    for i in range(6):
+        Ltemp.append(math.ceil(60 * random.random()))
+
+    Palpites = Ltemp[:]
+
+print(Palpites)
